88a72fad6804509b9adc795f2f85600df4620bc8
rajaaghanem <rajaaghanem@gmail.com>
2022-02-01 09:51:25 +0200

    add about us text

--- food-workshop/src/components/Pages/AboutUsPage/AboutUsPage.js
+++ food-workshop/src/components/Pages/AboutUsPage/AboutUsPage.js
@@ -8,22 +8,16 @@ function AboutUsPage() {
       <div className="about-us_container_content">
         <h1>ABOUT US</h1>
         <p>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-          1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-          Evil) by Cicero, written in 45 BC. This book is a treatise on the
-          theory of ethics, very popular during the Renaissance. The first line
-          of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
-          section 1.10.32. The standard chunk of Lorem Ipsum used since the
-          1500s is reproduced below for those interested. Sections 1.10.32 and
-          1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-          reproduced in their exact original form, accompanied by English
-          versions from the 1914 translation by H. Rackham.
+          I'm Pnina, a religious Druze woman from the village of Maghar, where I
+          was born and raised. I was widowed at a young age and was left to
+          raise my three small children on my own. I learned to cook traditional
+          Druze recipes as a girl, spending long days in the kitchen with my
+          mother and grandmother. Today, I still cook meals for my grown
+          children daily, and continue to prepare my family's traditional
+          recipes, passed down lovingly through the generations of women. I use
+          fresh fruits, vegetables and nuts that grow in my garden in every
+          dish. I would love to host you in my home, to share a meal and amazing
+          experiences.
         </p>
       </div>
     </div>
